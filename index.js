87cc8f70599da37537143f47cfb3968dba1f6032
jaehee <baskinhee@gmail.com>
2022-11-22 23:18:26 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -2,17 +2,35 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://jaehee:123456789a@firstone.et5n8ni.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlparser: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlparser: true,
+  })
   .then(() => console.log("mongoDB connected"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send());
+
+//회원가입을 위함
+app.post("/api/user/register", (req, res) => {
+  const user = new User(res.body);
+  //회원가입할 때 필요한 정보를 client에서 가져온다
+  //정보를 데이터베이스에 넣는다
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port${port}`));

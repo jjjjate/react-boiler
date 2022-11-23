cfd8ad56b69c0c6d4ba4f3e78661a1fa62988dea
Jaehee Han <baskinhee@rtruesoft.kr>
2022-11-23 10:42:03 +0900

    로그인 페이지 구현

--- models/User.js
+++ models/User.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    maxlength: 50,
+    maxlength: 100,
   },
   email: {
     type: String,
@@ -11,7 +15,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    maxlength: 50,
+    maxlength: 200,
   },
   lastname: {
     type: String,
@@ -29,6 +33,40 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre('save', function(next){
+  const user = this;
+  if(user.isModified("password")){
+    //비밀번호를 암호화
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if(err) return next(err);
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if(err) return next(err);
+        user.password = hash;
+        next()
+      })
+    })
+  } else {
+    next()
+  }
+});
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if(err) return cb(err);
+      cb(null, isMatch);
+  })
+}
+
+userSchema.methods.generateToken = function(cb) {
+  const user = this;
+  const token = jwt.sign(user._id.toHexString(), "sectetToken");
+  user.token = token;
+  user.save(function(err, user) {
+    if(err) return cb(err);
+    cb(null, user);
+  })
+}
 
 const User = mongoose.model("User", userSchema);
 
